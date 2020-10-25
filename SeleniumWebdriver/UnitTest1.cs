6fb312fd04c06c9006faa5bf484a99660d85100d
Jakub Vlcek <jawlcek@gmail.com>
2020-10-25 17:18:45 +0100

    test url changed

--- SeleniumWebdriver/UnitTest1.cs
+++ SeleniumWebdriver/UnitTest1.cs
@@ -14,7 +14,7 @@ namespace SeleniumWebdriver
         public void TestMethod1()
         {
             IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("http://localhost:5001/"); // this will open the web page
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php"); // this will open the web page
             driver.Close(); //this will close the browser session
             driver.Quit(); // this will stop the webdriver
         }
