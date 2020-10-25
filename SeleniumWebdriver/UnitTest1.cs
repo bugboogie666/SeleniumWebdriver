533e73100ca6c8c46d6f1b8afcdc9855f192e974
Jakub Vlcek <jawlcek@gmail.com>
2020-10-25 17:03:49 +0100

    Add project files.

---
+++ SeleniumWebdriver/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace SeleniumWebdriver
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://localhost:5001/"); // this will open the web page
+            driver.Close(); //this will close the browser session
+            driver.Quit(); // this will stop the webdriver
+        }
+    }
+}
